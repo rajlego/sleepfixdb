0b9786f4fa47e3915dbbb75dd2b11ed00adda543
rajlego <rajlego@gmail.com>
2021-04-27 13:20:55 +0900

    Update

--- docs/index.md
+++ docs/index.md
@@ -5,4 +5,5 @@ Publish your public notes with MkDocs
 ## Hello World!
 
 The `index.md` in the `/docs` folder is the homepage you see here.
+[[test]]
 
---
+++ sm-main-commit.bat
@@ -0,0 +1,5 @@
+CHOICE /C YNC /M "Press Y to UPLOAD N for No or C for Cancel."
+git add -A && git commit -m "Update"
+git push
+ECHO done uploading
+PAUSE
